320af4518242d727d79ecc97a4900d541d0cddd2
Paul <yaguachi.marcelo@gmail.com>
2022-09-23 16:33:11 -0500

    Se agrega manejo de Box

--- JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
+++ JetpackComposeCatalogo/app/src/main/java/com/example/jetpackcomposecatalogo/MainActivity.kt
@@ -4,10 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyComplexLayout()
                 }
             }
         }
@@ -37,15 +37,154 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
+fun MyComplexLayout() {
+    Column() {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Cyan),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Ejemplo 1",
+                color = Color.White
+            )
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Red),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Ejemplo 2",
+
+                    color = Color.White
+                )
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxHeight()
+                    .weight(1f)
+                    .background(Color.Green),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Ejemplo 3",
+                    color = Color.White
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .background(Color.Magenta),
+            contentAlignment = Alignment.BottomCenter
+        ) {
+            Text(
+                text = "Ejemplo 4",
+                color = Color.White
+            )
+        }
+    }
+}
+
+
+@Composable
+fun MyRow() {
+//    Row(
+//        modifier = Modifier
+//            .fillMaxSize(), horizontalArrangement = Arrangement.SpaceBetween
+//    ) {
+//        Text(
+//            text = "Ejemplo 1"
+//        )
+//        Text(
+//            text = "Ejemplo 1"
+//        )
+//        Text(
+//            text = "Ejemplo 1"
+//        )
+//    }
+
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .horizontalScroll(rememberScrollState())
+    ) {
+        Text(
+            text = "Ejemplo 1", modifier = Modifier.width(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1", modifier = Modifier.width(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1", modifier = Modifier.width(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1", modifier = Modifier.width(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1", modifier = Modifier.width(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1", modifier = Modifier.width(100.dp)
+        )
+    }
+}
+
+@Composable
+fun MyColumn() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            text = "Ejemplo 1",
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1",
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+        Text(
+            text = "Ejemplo 1",
+            modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(100.dp)
+        )
+
+    }
+}
+
+@Composable
+fun MyBox() {
     Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
         Box(
             modifier = Modifier
-                .width(50.dp)
-                .height(50.dp)
+                .width(200.dp)
+                .height(200.dp)
                 .background(Color.Cyan)
+                .verticalScroll(rememberScrollState()), contentAlignment = Alignment.Center
         ) {
-
+            Text(text = "Esto es un Ejemplo")
         }
     }
 }
@@ -54,6 +193,9 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview() {
     JetpackComposeCatalogoTheme {
-        Greeting("Android")
+//        MyBox()
+//        MyColumn()
+//        MyRow()
+        MyComplexLayout()
     }
 }
